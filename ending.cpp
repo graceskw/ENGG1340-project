6a49b0eb564f2ae85f8958672cb7be1088a41360
graceskw <graceskw@connect.hku.hk>
2022-05-05 12:17:01 +0800

    fixed bug in ending

--- ending.cpp
+++ ending.cpp
@@ -8,15 +8,14 @@
 #include "events.h"
 #include "roll.h"
 #include "scenes.h"
-#include "roll.cpp"         // for testing
-#include "events.cpp"       // for testing
+// #include "roll.cpp"         // for testing
+// #include "events.cpp"       // for testing
 using namespace std;
 
 
 void ending1(EventProgress eventprogress){     // Good ending
     // evidence
     if(eventprogress.event1){
-    
         cout << "The newspaper. ";
     }
     if(eventprogress.event2){
@@ -31,6 +30,13 @@ void ending1(EventProgress eventprogress){     // Good ending
     if(eventprogress.event5){
         cout << "The diary. ";
     }
+
+    // // for testing
+    //     cout << "The newspaper. ";
+    //     cout << "The fireplace. ";
+    //     cout << "The empty knife holder and the instructions. ";
+    //     cout << "The Kit chocolate. ";
+    //     cout << "The diary. ";
 
     cout << "\n\nIt all links up now.\n" << endl;
     cout << "You can feel the fury kindling within you. The love you once had for Asher now becomes pure hatred, so pure it is the perfect fuel for your growing desire. You desire to burn this house AND this man into ashes." << endl;
@@ -68,33 +74,33 @@ void ending1(EventProgress eventprogress){     // Good ending
     cout << "You picked up the item, rubbing your sore foot.\nEngravings of dots. This must be the dice that Hestia keeps fidgeting." << endl;
 
     cout << "Roll a dice to determine your action (>=3) (type dice to roll)" << endl;
-    roll_dice(0);
-
-    cout << "\nYou look around. \"Hest?\", you shout." << endl;
-    sleep(2);
-    cout << "No response.\n\"Hestia!\" you scream out her name." << endl;
-    cout << "\nReturning your devastating call is a burst of red flames at the door." << endl;
-    cout << "You rush to the flames, reaching out your hand to the warmth--" << endl;
-    sleep(1);
-    cout << "Flames touch the tips of your fingers.\nIt feels fuzzy and nostalgic, feels like hearth and hestia";
-    sleep(1);
-    cout << "The fire slowly spread to palms, crawing up your arms, scorching your body.\nIt getting too hot. You can feel  Hestia\'s fury." << endl;
-    sleep(1);
-    cout << "Before long, you are completelt engulfed in flames.\nYou can clearly feel Hestia\'s wrath through the burning pain on your skin." << endl;
-    sleep(1);
-    cout << "Hestia, standing at door. \nHer amber eyes reflecting the flickering flames, his pathetic begging figure being the centrepiece." << endl;
-    cout << "She smiles and leaves, the door closing behind her, closing the fate of the man who betrayed her." << endl;
     
-    sleep(2);    
-    cout << ". . ." << endl;
-    sleep(2);
+    if(roll_dice(0)){
+        cout << "\nYou look around. \"Hest?\", you shout." << endl;
+        sleep(2);
+        cout << "No response.\n\"Hestia!\" you scream out her name." << endl;
+        cout << "\nReturning your devastating call is a burst of red flames at the door." << endl;
+        cout << "You rush to the flames, reaching out your hand to the warmth--" << endl;
+        sleep(1);
+        cout << "Flames touch the tips of your fingers.\nIt feels fuzzy and nostalgic, feels like hearth and hestia";
+        sleep(1);
+        cout << "The fire slowly spread to palms, crawing up your arms, scorching your body.\nIt getting too hot. You can feel  Hestia\'s fury." << endl;
+        sleep(1);
+        cout << "Before long, you are completelt engulfed in flames.\nYou can clearly feel Hestia\'s wrath through the burning pain on your skin." << endl;
+        sleep(1);
+        cout << "Hestia, standing at door. \nHer amber eyes reflecting the flickering flames, his pathetic begging figure being the centrepiece." << endl;
+        cout << "She smiles and leaves, the door closing behind her, closing the fate of the man who betrayed her." << endl;
+        
+        sleep(2);    
+        cout << ". . ." << endl;
+        sleep(2);
 
-    cout << "Ashes fills the air, spiralling lavishly under the dawning sun. \nFlames furiously flickering, dancing atrociously until the forest regained its silence." << endl;
+        cout << "Ashes fills the air, spiralling lavishly under the dawning sun. \nFlames furiously flickering, dancing atrociously until the forest regained its silence." << endl;
 
-    sleep(2);    
-    cout << "The end." << endl;
-    sleep(2);
-
+        sleep(2);    
+        cout << "The end." << endl;
+        sleep(2);
+    }
 }
 
 void ending2(){     // Bad ending
@@ -123,7 +129,7 @@ void final(EventProgress eventprogress){
     }
 
     if(progress >= 3){                      // the ending is dependent on the number of successful events
-        ending1();
+        ending1(eventprogress);
     }
     else{
         ending2();
@@ -131,6 +137,6 @@ void final(EventProgress eventprogress){
 }
 
 // for debugging
-int main(){
-    ending1();
-}
+// int main(){
+//     ending1(eventprogress);
+// }
